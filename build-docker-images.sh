d6626ed2cd3e8a3622b549bb8f1e220af26bec50
gt <gtiwari333@gmail.com>
2026-06-06 13:20:52 -0500

    more tests, fixed flaky e2e tests, use mysql for integration test

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-# full build with 5 threads, skipping test
-mvnd  -T 5 clean package install  -DskipTests=true
+# Enable testcontainers reuse
+TC_PROPS="$HOME/.testcontainers.properties"
+if ! grep -q "testcontainers.reuse.enable=true" "$TC_PROPS" 2>/dev/null; then
+    echo "testcontainers.reuse.enable=true" >> "$TC_PROPS"
+    echo "Set testcontainers.reuse.enable=true in $TC_PROPS"
+fi
 
 # create docker images out of build jar files using spring boot and packeto
-# the 'build-image-no-fork' expects the project is already built and .jar file already exists in target dir
 
-mvnd spring-boot:build-image-no-fork -pl content-checker/content-checker-service
-mvnd spring-boot:build-image-no-fork -pl email/email-service
-mvnd spring-boot:build-image-no-fork -pl trend/trend-service
-mvnd spring-boot:build-image-no-fork -pl main-app/main-webapp
-mvnd spring-boot:build-image-no-fork -pl main-app/report-service
+mvnd spring-boot:build-image -pl content-checker/content-checker-service -DskipTests=true
+mvnd spring-boot:build-image -pl email/email-service -DskipTests=true
+mvnd spring-boot:build-image -pl trend/trend-service -DskipTests=true
+mvnd spring-boot:build-image -pl main-app/main-webapp -DskipTests=true
+mvnd spring-boot:build-image -pl main-app/report-service -DskipTests=true
 
 # list
 docker image ls | grep gtapp
